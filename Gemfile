0888a1ab3cf7251013611bb60e7f38a0fbe93c25
Phill Conrad <pconrad@cs.ucsb.edu>
2018-06-22 12:05:56 -0700

    starting point

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source 'http://rubygems.org'
+ruby "2.4.2"
+gem 'github-pages'
---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "Installing software needed to run Jekyll locally... "
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" 
+
+rvm install ruby-2.4.2
+rvm use 2.4.2
+gem install bundler
+bundle install --path vendor/bundle
+echo "Done."
